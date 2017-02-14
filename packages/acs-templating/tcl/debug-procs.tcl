31174393180c80c1de45ea46dbe456685873b4cf
Iuri Sampaio <iuri@iurix.com>
2017-02-14 09:46:04 -0200

    fixed checkbox horizontal layout

---
+++ packages/acs-templating/tcl/debug-procs.tcl
@@ -0,0 +1,28 @@
+
+ad_proc -public watch_files {} {
+    tracks each file by interpreter to ensure that it is up-to-date
+} {
+  
+  set files [list ats/paginator-procs.tcl ats/query-procs.tcl \
+                  ats/debug-procs.tcl ats/filter-procs.tcl ats/util-procs.tcl]
+
+  foreach file $files { 
+
+    set file $::acs::tcllib/$file
+
+    set proc_name [info commands ::template::mtimes::tcl::$file]
+    set mtime [file mtime $file]
+
+    if { $proc_name eq {} || $mtime != [$proc_name] } {
+
+      uplevel #0 "source $file"
+      proc ::template::mtimes::tcl::$file {} "return $mtime"
+    }
+  }
+}
+
+# Local variables:
+#    mode: tcl
+#    tcl-indent-level: 4
+#    indent-tabs-mode: nil
+# End:
---
+++ packages/acs-templating/www/doc/demo/form.adp
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Demo: Form</title>
+  <style>
+    h1 { font-family: Arial, Helvetica }
+    th { font-family: Arial, Helvetica }
+    td { font-family: Arial, Helvetica }
+  </style>
+  </head>
+  <body bgcolor="#FFFFCC">
+  <h1>Add a User</h1>
+<hr>
+  <formtemplate id="add_user"></formtemplate>
+<hr>
+</body>
+</html>
