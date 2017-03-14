87985b23ae8a26f5fb098d8c189ed71002b40b96
Iuri Sampaio <iuri@iurix.com>
2017-03-14 21:39:02 +0000

    fixed plain-master

--- packages/1c-theme/lib/plain-master.tcl
+++ packages/1c-theme/lib/plain-master.tcl
@@ -9,3 +9,11 @@ template::head::add_css -href "/resources/1c-theme/css/stylesheet.css" -order 1
 template::head::add_javascript -src "https://code.jquery.com/jquery-2.2.4.min.js" -order 0
 template::head::add_javascript -src "/resources/1c-theme/js/metro.js" -order 1
 template::head::add_javascript -src "/resources/1c-theme/js/select2.js" -order 1
+
+
+
+
+
+if {![info exists skip_link]} {
+    set skip_link "#content-wrapper"
+}
