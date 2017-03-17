4bfb5065d440719b16771cba624775667f2195d9
Iuri Sampaio <iuri@iurix.com>
2017-03-17 10:11:06 -0300

    before push to repos

--- packages/1c-realties/lib/charaq-req.adp
+++ packages/1c-realties/lib/charaq-req.adp
@@ -1,19 +1,5 @@
 <!-- Características do matching -->
 
 
-<multiple name='characs'>
-  <div class='box2' >
-  	<b>@characs.cat_name@</b><br>
 
-
-	<multiple name='options'>
-	  
-  	  <label class='input-control radio small-check' >
-	    <input type='radio' name='charreq_@characs.cat_id@' id='charreq_@characs.cat_id@' />
-	    <span class='check' ></span>
-	    <span class='caption' >@options.value@</span>
-	  </label>
-	  
-	</multiple>
-  </div>
-</multiple>
+	@chars_html;noquote@
--- packages/1c-realties/lib/charaq-req.tcl
+++ packages/1c-realties/lib/charaq-req.tcl
@@ -2,30 +2,66 @@ ad_page_contract {
 
 }
 
-set package_id 2393
+
+ad_proc -public category_get_options {
+    {-parent_id:required}
+} {
+    @return Returns the category types for this instance as an
+    array-list of { parent_id1 heading1 parent_id2 heading2 ... }
+} {
+
+    set children_ids [category::get_children -category_id $parent_id]
+
+    set children [list]
+    foreach child_id $children_ids {
+	set child_name [category::get_name $child_id]
+	set temp "\"$child_name\" $child_id"
+	lappend children $temp
+    }
+    
+    lappend children ""
+
+    return $children
+}
+
+
+
+set package_id 2372
 set tree_id 1067
 
-multirow create characs cat_id cat_name
+
+set chars_html ""
 
 foreach {cat_id cat_name} [1c_annonce::get_categories -package_id $package_id -tree_id $tree_id] {
 
-    if {[template::multirow exists options]} {
-	template::multirow unset options
-    }
+    
+    append chars_html "
 
-    template::multirow extend options value
-   
-
+      <div class='box2' >
+  	<b>$cat_name</b><br> 
+    "
+    
+    set options_html ""
     
     ns_log Notice "$cat_id $cat_name"
     
-    multirow extend  -unclobber options select_options [1c_annonce::category_get_options -parent_id $cat_id] {
+    foreach option [category_get_options -parent_id $cat_id] {
 	
-	set value [lindex $elem 0]
-		
+	ns_log Notice "[lindex $option 0]"
+	set option [lindex $option 0]
+	append options_html "
+	    <label class='input-control radio small-check' >
+	    <input type='radio' name='charreq_$cat_id' id='$cat_id' />
+	    <span class='check' ></span>
+	    <span class='caption' >$option</span>
+	    </label>
+	    
+	"
     }
-    
-    multirow append characs $cat_id $cat_name
+
+    append chars_html "$options_html </div>"
+
+    ns_log Notice "$options_html"
     
 }
 
