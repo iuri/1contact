4ccf607f9f07fbbdfe5255581178f0b366998440
Iuri <iuri@iurix.com>
2017-03-17 17:20:30 -0300

    created annonce form validation

---
+++ packages/1c-annonces/www/resources/js/create_annonce_form.js
@@ -0,0 +1,25 @@
+function form_submit() {
+
+	var announce_title = $('#announce_title').val();
+	var announce_business = $('#announce_business').val();
+	var announce_type = $('#announce_type').val();
+	var announce_price = $('#announce_price').val();
+	var announce_taxes = $('#announce_taxes').val();
+	var announce_availability = $('#announce_availability').val();
+	var announce_rooms = $('#announce_rooms').val();
+	var announce_lavatory = $('#announce_lavatory').val();
+	var announce_bathroom = $('#announce_bathroom').val();
+	var announce_floorsnmb = $('#announce_floorsnmb').val();
+	var announce_surface = $('#announce_surface').val();
+	var announce_announcer = $('#announce_announcer').val();
+	
+	var radio = $('#charreq_1090').filter(':checked').data("title");
+
+	var announce_business = $('#announce_business').val();
+	var announce_business = $('#announce_business').val();
+	var announce_business = $('#announce_business').val();
+	var announce_business = $('#announce_business').val();
+
+	alert(radio);
+
+}
